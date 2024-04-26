42bc64fc441a5aed8ed1d5950809a809b00c74f3
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-04-26 17:39:55 +0530

    Create 1.ReverseString.js

---
+++ JavaScript_Logical_Part/1.ReverseString.js
@@ -0,0 +1,12 @@
+var Emptyvariable=""
+var varname="javascript"
+
+for(var i=varname.length;i>=0;i--){
+   Emptyvariable=Emptyvariable+varname.charAt(i)
+}
+console.log("The Given String is----------->"+varname)
+console.log("The Reversed String----------->"+Emptyvariable)
+
+output:-
+  The Given String is----------->javascript
+  The Reversed String----------->tpircsavaj
